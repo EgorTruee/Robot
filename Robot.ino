647e0ab13f87c2a41d676fac0aed6b0fb904ec9c
EgorTruee <78207363+EgorTruee@users.noreply.github.com>
2025-05-21 22:30:12 +0300

    Scale fix

--- Robot.ino
+++ Robot.ino
@@ -43,7 +43,7 @@ const char index_html[] PROGMEM = R"rawliteral(
             max-width: 800px;
             margin: 0 auto;
             padding: 20px;
-			touch-action: non
+			touch-action: none
         }
         .slider-container {
 			margin-top: 150px;
